271ca47582d7b612bb3fdd2ae7ec183681943f7e
Hassan Ali <ihasanalyy@gmail.com>
2025-02-11 20:07:46 +0500

    main_menu_implement

--- controllers/RegisterUser.js
+++ controllers/RegisterUser.js
@@ -27,52 +27,145 @@
 //         await sendMessage(chatId, "Please enter your first name:");
 //     }
 
-import { sendPhoto,sendMessage } from "../utils/messageHelper.js";
+import { sendPhoto,sendMessage, sendButtons } from "../utils/messageHelper.js";
 
 // }
 
-export async function registerUser(chatId, message, callback_query, text, currentStep, req, res) {
-    console.log("we are in register foo")
-    console.log(currentStep, "currentStep")
-    console.log(callback_query, "callback_query")
-    console.log(message, "message")
-    console.log(chatId, "chatId")
-    console.log(text, "text")
-    console.log(req.body.callback_query, "req")
-    if (callback_query === "cancel_registration") {
-        const Buttons = [
-            [{ text: "Connect Account", callback_data: "connect_account" }], // Provide Connect button
-            [{ text: "Register", callback_data: "register" }], // Provide Register button
-            [{ text: "Change Language", callback_data: "language_change" }], // Provide Change Language button
+export async function registerUser(chatId,  payload, chat) {
+    console.log("we are in registerUSer foo")
+    // console.log(currentStep, "currentStep")
+    console.log(payload, "payload aya kia")
+    // console.log(message, "message")
+    // console.log(chatId, "chatId")
+    // console.log(text, "text")
+    // console.log(req.body.callback_query, "req")
+    // if (callback_query === "cancel_registration" || chat.last_message?.startsWith("step1")) {
+    //     const Buttons = [
+    //         [{ text: "Connect Account", callback_data: "connect_account" }], // Provide Connect button
+    //         [{ text: "Register", callback_data: "register" }], // Provide Register button
+    //         [{ text: "Change Language", callback_data: "language_change" }], // Provide Change Language button
+    //     ];
+    //     // await sendMessage(chatId, "Registration Cancelled", Buttons, "connect");
+    //     await sendButtons(chatId, "Registration Cancelled", Buttons, "connect");
+    // } else if (callback_query === "template_registration" || chat.last_message?.startsWith("connect")){
+    //     const Buttons = [
+    //         [{ text: "Connect Account", callback_data: "connect_account" }], // Provide Connect button
+    //         [{ text: "Register", callback_data: "register" }], // Provide Register button
+    //         [{ text: "Change Language", callback_data: "language_change" }], // Provide Change Language button
+    //     ];
+    //     // await sendMessage(chatId, "Registration Cancelled", Buttons, "connect");
+    //     await sendButtons(chatId, "Registration Cancelled", Buttons, "register00");
+    // }
+    // else if (callback_query === "register" || chat.last_message === "register00") {
+    //     console.log("hum register main hain")
+    //     const Buttons = [
+    //         [{ text: "Individual", callback_data: "register_individual" }], // Provide Individual button
+    //         [{ text: "Business", callback_data: "register_business" }], // Provide Business button
+    //         [{ text: "Back", callback_data: "back" }], // Provide Back button
+    //     ];
+    //     await sendPhoto(chatId, "https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705350_640.jpg", "Great! Are you signing up as an individual or a business?");
+    //     // await sendMessage(chatId, "Please select an option:", "registerFlow0");
+    //     await sendButtons(chatId, "Please select an option:", Buttons, "registerFlow0");
+    // } 
+    // else if (callback_query === "register_template") {
+    //     // Handle template registration
+    //     const Buttons = [
+    //         [{ text: "Connect Account", callback_data: "connect_account" }], // Provide Connect button
+    //         [{ text: "Register", callback_data: "register" }], // Provide Register button
+    //         [{ text: "Change Language", callback_data: "language_change" }], // Provide Change Language button
+    //     ];
+    //     await sendMessage(chatId, "Template registration is not available at the moment.", Buttons, "templateFlow");
+    // } 
+    // else if (callback_query === "register_individual") {
+    //     console.log("we are in register individual")
+    //     req.app.locals.flows = req.app.locals.flows || {};
+    //     req.app.locals.flows[chatId] = { step: 1, userType: "individual" };
+    //     const Buttons = [
+    //         [{ text: "Cancel Registration", callback_data: "cancel_registration" }],
+    //      ] // Provide Back button
+    //     await sendPhoto(chatId, "https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705350_640.jpg");
+    //     await sendMessage(chatId, "Please enter your first name:", Buttons, "registerFlow1");
+    // }
+    if (payload === "register_cancel" && chat.last_message?.startsWith("register")) {
+        console.log("we are in register cancel")
+        const buttonText = "Registration Cancelled";
+        const buttons = [
+            [{ text: "Connect Account", callback_data: "connect_account" }],
+            [{ text: "Register", callback_data: "register" }],
+            [{ text: "Change Language", callback_data: "language_change" }],
         ];
-        await sendMessage(chatId, "Registration Cancelled", Buttons, "connect");
-    } 
-    else if (callback_query === "register") {
-        const Buttons = [
-            [{ text: "Individual", callback_data: "register_individual" }], // Provide Individual button
-            [{ text: "Business", callback_data: "register_business" }], // Provide Business button
-            [{ text: "Back", callback_data: "back" }], // Provide Back button
-        ];
-        await sendPhoto(chatId, "https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705350_640.jpg", "Great! Are you signing up as an individual or a business?");
-        await sendMessage(chatId, "Please select an option:", Buttons, "registerFlow0");
-    } 
-    else if (callback_query === "register_template") {
-        // Handle template registration
-        const Buttons = [
-            [{ text: "Connect Account", callback_data: "connect_account" }], // Provide Connect button
-            [{ text: "Register", callback_data: "register" }], // Provide Register button
-            [{ text: "Change Language", callback_data: "language_change" }], // Provide Change Language button
-        ];
-        await sendMessage(chatId, "Template registration is not available at the moment.", Buttons, "templateFlow");
-    } 
-    else if (callback_query === "register_individual") {
-        console.log("we are in register individual")
-        req.app.locals.flows = req.app.locals.flows || {};
-        req.app.locals.flows[chatId] = { step: 1, userType: "individual" };
-        const Buttons = [
-            [{ text: "Cancel Registration", callback_data: "cancel_registration" }],
-         ] // Provide Back button
-        await sendPhoto(chatId, "https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705350_640.jpg");
-        await sendMessage(chatId, "Please enter your first name:", Buttons, "registerFlow1");
+        await sendButtons(chatId, buttons, buttonText, "connect");
     }
+    else if (payload === "register_template" && chat.last_message?.startsWith("connect")) {
+        console.log("we are in register template") 
+        const buttons = [
+            [{ text: "Connect Account", callback_data: "connect_account" }],
+            [{ text: "Register", callback_data: "register" }],
+            [{ text: "Change Language", callback_data: "language_change" }],
+        ];
+        await sendButtons(chatId, buttons,"How can we help you today? Let's get started!🚀👇",  "register000");
+    }
+    else if (payload === "register" && chat.last_message === "register") {
+        console.log("we are in register")
+        const buttonText = "Great! Are you signing up as an individual or a business?";
+        const buttons = [
+            [{ text: "Account Register Ind", callback_data: "register_acc_ind" }],
+            [{ text: "Account Register Bus", callback_data: "register_acc_bus" }],
+            [{ text: "Main Menu", callback_data: "main_menu" }],
+        ];
+        await sendPhoto(chatId, "https://nodejs-checking-bucket.s3.amazonaws.com/telegram_bot_images/Select.png");
+
+        await sendButtons(chatId,buttons, buttonText,  "register_0");
+    } else if (payload === "register_acc_ind" && chat.last_message === "register_0") {
+        console.log("we are in register individual")
+        const buttons = [
+            [{ text: "Cancel Registration", callback_data: "register_cancel" }],
+        ];
+        await sendPhoto(chatId, "https://nodejs-checking-bucket.s3.amazonaws.com/telegram_bot_images/Individual.png");
+        await sendMessage(chatId, "Please enter your first name:", buttons, "register_1");
+    } else if (payload === "register_acc_bus" && chat.last_message === "register_1") {
+        console.log("we are in register business")
+        const buttons = [
+            [{ text: "Cancel Registration", callback_data: "register_cancel" }],
+        ];
+        await sendPhoto(chatId, "https://nodejs-checking-bucket.s3.amazonaws.com/telegram_bot_images/Business.png");
+        await sendMessage(chatId, "Business account registration will come soon.", buttons, "register_2");
+    } else if (payload === "main_menu" || chat.last_message === "register_0") {
+        console.log("Going to main menu");
+        const message = "Welcome back! Need to make a transaction? Select from the options below🚀👇";
+        await sendMessage(chatId, message);
+        
+        // wallet overview
+        const walletMessage = "View your balance and recent activity";
+        await sendPhoto(chatId, "https://nodejs-checking-bucket.s3.amazonaws.com/telegram_bot_images/Select.png");
+        const buttons = [
+            [{ text: "Wallet Overview", callback_data: "wallet_overview" }],
+        ];
+        await sendButtons(chatId,buttons,walletMessage,"wallet_overview");
+
+        // Initiate Payment
+        const paymentMessage = "Send money quickly and securely";
+        await sendPhoto(chatId, "https://media.istockphoto.com/id/1030875166/photo/sending-money.jpg?s=1024x1024&w=is&k=20&c=jMo8Sy1X7lhpGv9IMLTFxZvdTIfQ_ftiggwcNhg5nOQ=");
+        const paymentButtons = [
+            [{ text: "Initiate Payment", callback_data: "initiate_payment" }],
+        ];
+        await sendButtons(chatId,paymentButtons,paymentMessage,"initiate_payment");
+
+        // My Transactions
+        const transactionMessage = "Review your past transaction";
+        await sendPhoto(chatId, "https://nodejs-checking-bucket.s3.amazonaws.com/telegram_bot_images/Select.png");
+        const transactionButtons = [
+            [{ text: "My Transactions", callback_data: "my_transactions" }],
+        ];
+        await sendButtons(chatId,transactionButtons,transactionMessage,"my_transactions");
+
+        // QR Quick Pay
+        const qrMessage = "Pay using a QR code instantly";
+        await sendPhoto(chatId, "https://cdn.vectorstock.com/i/500p/26/70/qr-code-sample-for-smartphone-scanning-3d-web-vector-43412670.jpg");
+        const qrButtons = [
+            [{ text: "QR Quick Pay", callback_data: "qr_quick_pay" }],
+        ]
+        await sendButtons(chatId,qrButtons,qrMessage,"qr_quick_pay");
+    }
+    
 }
--- utils/messageHelper.js
+++ utils/messageHelper.js
@@ -1,28 +1,27 @@
 import axios from "axios";
 import { teleGramAPI } from "../config/botConfig.js";
+import { TelegramBot } from "../models/User.js";
 
-export const sendMessage = async (chatId, text, Buttons = null, userFlow = null, req = null) => {
+export const sendMessage = async (chatId, text, lastMessage) => {
     const payload = {
         chat_id: chatId,
         text,
-        parse_mode: "Markdown", // Optional for better formatting
     };
 
-    // Buttons ko sahi format me bhejna
-    if (Buttons) {
-        payload.reply_markup = {
-            inline_keyboard: Buttons, // Telegram expects buttons inside `reply_markup`
-        };
-    }
+    // // Buttons ko sahi format me bhejna
+    // if (Buttons) {
+    //     payload.reply_markup = {
+    //         inline_keyboard: Buttons, // Telegram expects buttons inside `reply_markup`
+    //     };
+    // }
 
     try {
         const response = await axios.post(`${teleGramAPI}/sendMessage`, payload);
         console.log("Message sent successfully:", response.data);
 
         // Agar userFlow diya gaya hai, to next step ko update karo
-        if (userFlow && req) {
-            req.app.locals.currentStep = userFlow;
-            console.log(`User flow updated to: ${userFlow}`);
+        if (lastMessage) {
+            await updateLastMessage(chatId, lastMessage);
         }
 
     } catch (error) {
@@ -45,3 +44,37 @@ export const sendPhoto = async (chatId, photoUrl, caption, options) => {
         console.error("Error sending photo:", error.response?.data || error.message);
     }
 };
+
+export const updateLastMessage = async (chatId, lastMessage) => {
+    console.log("Updating last message:", lastMessage);
+    try {
+        const telegramBot = await TelegramBot.findOneAndUpdate(
+            { recipient: chatId }, // Find by chatId
+            { last_message: lastMessage }, // Update last_message
+            { upsert: true, new: true } // Create if not exists (upsert)
+        );
+
+        console.log("Updated document:", telegramBot);
+    } catch (error) {
+        console.error("Error updating last message:", error.response?.data || error.message);
+    }
+};
+
+ export async function sendButtons(chatId, Buttons,text, lastMessage) {
+    console.log("we are in sendButtons")
+    try {
+        const response = await axios.post(`${teleGramAPI}/sendMessage`, {
+            chat_id: chatId,
+            text: text,
+            reply_markup: {
+                inline_keyboard: Buttons,
+            },
+        });
+        console.log('Buttons sent:', response.data);
+        if (lastMessage) {
+            await updateLastMessage(chatId, lastMessage);
+        }
+    } catch (error) {
+        console.error('Error sending buttons:', error.response?.data || error.message);
+    }
+}
